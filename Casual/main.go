9ae8da08615f071ec2b4c29a94753a05bedf2741
Serhat Abdulbakioğlu <serhat359@gmail.com>
2019-10-28 06:44:58 +0300

    added configuration file to the project

--- Casual/main.go
+++ Casual/main.go
@@ -5,6 +5,9 @@ import (
 	"net/http"
 	"fmt"
 	"reflect"
+	"os"
+	"encoding/json"
+	"strconv"
 )
 
 type A struct {
@@ -12,10 +15,32 @@ type A struct {
 	Foo2 int
 }
 
-func main() {
+type Configuration struct {
+    Port int `json:"port"`
+}
+
+func main(){
+	runServer()
+}
+
+func runServer() {
 	router := NewRouter()
-	server := http.ListenAndServeTLS(":24672", "server.crt", "server.key", router) // 24672 for development port
+	port := getPort()
+	server := http.ListenAndServeTLS(port, "server.crt", "server.key", router) // 24672 for development port, 36219 for actual service
 	log.Fatal(server)
+}
+
+func getPort() string {
+	file, _ := os.Open("conf.json")
+	defer file.Close()
+
+	decoder := json.NewDecoder(file)
+	configuration := Configuration{}
+	err := decoder.Decode(&configuration)
+	if err != nil {
+		panic(err)
+	}
+	return ":" + strconv.Itoa(configuration.Port)
 }
 
 func testReflection(){
